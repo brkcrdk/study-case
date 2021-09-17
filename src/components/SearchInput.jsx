63ececc9fc6fa5db418862bff86bac87133e84f3
Burak <burak@90pixel.com>
2021-09-17 22:30:10 +0300

    Add search input component.

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,8 +1,34 @@
 import styled from "styled-components";
+import { Icon } from "components";
 
 function SearchInput(props) {
-  return <input {...props} />;
+  return (
+    <Wrapper>
+      <Icon name="search" size={18} />
+      <CustomInput {...props} />
+    </Wrapper>
+  );
 }
 export default SearchInput;
 
-const CustomInput = styled.input``;
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 695px;
+  height: 48px;
+  .icon {
+    position: absolute;
+    left: 27px;
+    top: 15px;
+  }
+`;
+
+const CustomInput = styled.input`
+  width: 100%;
+  font-size: 15px;
+  background: #eee;
+  border: none;
+  height: 100%;
+  padding-left: 56px;
+  border-radius: 100px;
+`;
