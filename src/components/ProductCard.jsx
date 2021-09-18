5fdcb17d12c5e73d071aef550d7e210f6f3ef2e6
Burak <burak@90pixel.com>
2021-09-18 17:29:51 +0300

    Add product container component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,4 @@
+function ProductCard() {
+  return <div>ProductCard is here</div>;
+}
+export default ProductCard;
---
+++ src/components/ProductContainer.jsx
@@ -0,0 +1,4 @@
+function ProductContainer() {
+  return <div>ProductContainer is here</div>;
+}
+export default ProductContainer;
