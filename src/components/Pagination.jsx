c0a2a7511ce553ac1cfba50dc4b19c840ab5d947
Burak <burak@90pixel.com>
2021-09-18 21:45:48 +0300

    Add pagination component.

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,39 @@
+import styled from "styled-components";
+import { Icon } from "components";
+import { colors } from "theme";
 function Pagination() {
-  return <div>Pagination is here</div>;
+  return (
+    <PaginationWrapper>
+      <li className="prev">
+        <Icon size={6} name="left-icon" />
+      </li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li className="next">
+        <Icon size={6} name="right-icon" />
+      </li>
+    </PaginationWrapper>
+  );
 }
+
 export default Pagination;
+
+const PaginationWrapper = styled.ul`
+  margin-top: 32px;
+  display: flex;
+  align-items: center;
+  color: ${colors.navText};
+  li {
+    display: grid;
+    place-items: center;
+    margin: 0 4px;
+    border-radius: 8px;
+    border: 1px solid ${colors.paginationBorder};
+    padding: 6px;
+    width: 33px;
+    height: 33px;
+    cursor: pointer;
+    font-size: 10px;
+  }
+`;
