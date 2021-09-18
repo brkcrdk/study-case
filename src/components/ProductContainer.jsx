d58ae22c0a9a1b6bb0eb5e2cc9d98b04a81f1cde
Burak <burak@90pixel.com>
2021-09-18 21:47:01 +0300

    Add products to product container.

--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -1,4 +1,20 @@
+import styled from "styled-components";
+import data from "../data.json";
+import ProductCard from "./ProductCard/ProductCard";
+
 function ProductContainer() {
-  return <div>ProductContainer is here</div>;
+  return (
+    <ProductContainerWrapper>
+      {data.map((product, index) => (
+        <ProductCard key={`product-${index}`} />
+      ))}
+    </ProductContainerWrapper>
+  );
 }
 export default ProductContainer;
+
+const ProductContainerWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 16px;
+`;
