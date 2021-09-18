ed986721ba9de4310c883dae08e039bdbd3a8b96
Burak <burak@90pixel.com>
2021-09-19 00:38:39 +0300

    Add modal to app.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import GlobalStyle from "styles/globalStyles";
 import "styles/libraryStyles.css";
 import App from "./App";
+import { Providers } from "store";
+import { Modal } from "./components";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <Providers>
+      <App />
+      <GlobalStyle />
+      <Modal />
+    </Providers>
   </React.StrictMode>,
   document.getElementById("root")
 );
