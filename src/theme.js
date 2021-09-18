123ef82b76447d215cd7ae75ee9571aaf4477367
Burak <burak@90pixel.com>
2021-09-18 21:45:40 +0300

    Add new colors and lineClamp function to theme.

--- src/theme.js
+++ src/theme.js
@@ -3,4 +3,20 @@ export const colors = {
   lightGray: "#b0b0b0",
   darkGray: "#484848",
   navText: "#646464",
+  realPrice: "#9b9b9b",
+  cardBorder: "#cdcdcd",
+  productImageBorder: "#e5e5e5",
+  red: "#f90000",
+  disabledBtnBackground: "#f1f1f1",
+  btnBackground: "#ffeee3",
+  paginationBorder: "#eee",
+};
+
+export const lineClamp = (clampCount) => {
+  return `
+  display: -webkit-box;
+  -webkit-line-clamp: ${clampCount};
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  `;
 };
