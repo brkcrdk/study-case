eba2bb9e8633e08855c44d7719ec179e3ad87691
Burak <burak@90pixel.com>
2021-09-18 15:09:25 +0300

    Add products header to app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Header } from "components";
+import { Header, ProductsHeader, Container } from "components";
 
 function App() {
   return (
     <main data-testid="app">
       <Header />
+      <Container>
+        <ProductsHeader />
+      </Container>
     </main>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Icon } from "./Icon";
 export { default as SearchInput } from "./SearchInput";
 export { default as Cart } from "./Cart/Cart";
 export { default as Header } from "./Header";
+export { default as ProductsHeader } from "./ProductsHeader";
