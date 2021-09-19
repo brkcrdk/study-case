c6d08a7d6e0982b7af69d8c32c33bdc210035280
Burak <burak@90pixel.com>
2021-09-20 01:05:31 +0300

    Add sorting methods.

--- src/store/filterStore.js
+++ src/store/filterStore.js
@@ -7,6 +7,7 @@ const FilterProvider = ({ children }) => {
   const [data, setData] = useState(initialData);
   const [activePage, setActivePage] = useState(1);
   const [search, setSearch] = useState("");
+  const [sort, setSort] = useState({});
 
   const onSearch = (searchValue) => {
     setSearch(searchValue);
@@ -24,7 +25,37 @@ const FilterProvider = ({ children }) => {
 
   const onPageChange = (goTo) => setActivePage(goTo);
 
-  const values = { data, search, onSearch, activePage, onPageChange };
+  const onSortChange = (sortOption) => {
+    setSort(sortOption);
+    console.log(sortOption);
+    switch (sortOption) {
+      case "ASC": {
+        return data.sort((a, b) => a.name.localeCompare(b.name));
+      }
+      case "DESC": {
+        return data.sort((a, b) => b.name.localeCompare(a.name));
+      }
+      case "priceASC": {
+        return data.sort((a, b) => a.realPrice - b.realPrice);
+      }
+      case "priceDESC": {
+        return data.sort((a, b) => b.realPrice - a.realPrice);
+      }
+      default: {
+        return data;
+      }
+    }
+  };
+
+  const values = {
+    data,
+    search,
+    onSearch,
+    activePage,
+    onPageChange,
+    sort,
+    onSortChange,
+  };
 
   return (
     <FilterContext.Provider value={values}>{children}</FilterContext.Provider>
