22889ad5c7b397bcc377dc91d7747c39faa5c7b9
Burak <burak@90pixel.com>
2021-09-19 23:49:35 +0300

    Refactor usePagination hook.

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,21 +1,29 @@
 import { useContext } from "react";
 import { FilterContext } from "store";
 import { paginate } from "utils";
-import productData from "../data.json";
 
 const usePagination = () => {
-  const { activePage, onPageChange } = useContext(FilterContext);
-  const { data, pageCount } = paginate({ page: activePage, data: productData });
+  const { data, activePage, onPageChange } = useContext(FilterContext);
+  const { data: paginatedData, pageCount } = paginate({
+    page: activePage,
+    data,
+  });
 
   const handleNext = () => {
     if (activePage !== pageCount) return onPageChange(activePage + 1);
   };
-
   const handlePrev = () => {
     if (activePage !== 1) return onPageChange(activePage - 1);
   };
-
   const handleGoTo = (goTo) => onPageChange(goTo);
-  return { data, activePage, pageCount, handlePrev, handleNext, handleGoTo };
+
+  return {
+    data: paginatedData,
+    activePage,
+    pageCount,
+    handlePrev,
+    handleNext,
+    handleGoTo,
+  };
 };
 export default usePagination;
