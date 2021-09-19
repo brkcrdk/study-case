a8d5b2f86eb6ce7310574806364a0bf01af255b2
Burak <burak@90pixel.com>
2021-09-19 17:59:17 +0300

    Add pagination methods to pagination component.

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,18 +1,47 @@
-import styled from "styled-components";
+import { useContext } from "react";
+import styled, { css } from "styled-components";
 import { Icon } from "components";
 import { colors } from "theme";
+import { paginate } from "utils";
+import { FilterContext } from "store";
+
 function Pagination() {
+  const { activePage, onPageChange } = useContext(FilterContext);
+  const { pageCount } = paginate();
+
+  const handleNext = () => {
+    if (activePage !== pageCount) return onPageChange(activePage + 1);
+  };
+
+  const handlePrev = () => {
+    if (activePage !== 1) return onPageChange(activePage - 1);
+  };
+
   return (
     <PaginationWrapper>
-      <li className="prev">
-        <Icon size={6} name="left-icon" />
-      </li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li className="next">
-        <Icon size={6} name="right-icon" />
-      </li>
+      <PageItem
+        className="prev"
+        disabled={activePage === 1}
+        onClick={handlePrev}
+      >
+        <Icon size={8} name="left-icon" />
+      </PageItem>
+      {[...Array(pageCount).keys()].map((page, index) => (
+        <PageItem
+          className={activePage === page + 1 ? "active" : ""}
+          key={`pagination-page-${page}`}
+          onClick={() => onPageChange(page + 1)}
+        >
+          {page + 1}
+        </PageItem>
+      ))}
+      <PageItem
+        className="next"
+        disabled={activePage === pageCount}
+        onClick={handleNext}
+      >
+        <Icon size={8} name="right-icon" />
+      </PageItem>
     </PaginationWrapper>
   );
 }
@@ -24,16 +53,29 @@ const PaginationWrapper = styled.ul`
   display: flex;
   align-items: center;
   color: ${colors.navText};
-  li {
-    display: grid;
-    place-items: center;
-    margin: 0 4px;
-    border-radius: 8px;
-    border: 1px solid ${colors.paginationBorder};
-    padding: 6px;
-    width: 33px;
-    height: 33px;
-    cursor: pointer;
-    font-size: 10px;
+`;
+
+const PageItem = styled.li`
+  display: grid;
+  place-items: center;
+  margin: 0 4px;
+  border-radius: 8px;
+  border: 1px solid ${colors.paginationBorder};
+  padding: 6px;
+  width: 33px;
+  height: 33px;
+  cursor: pointer;
+  font-size: 12px;
+  &.active,
+  &.prev:hover,
+  &.next:hover {
+    font-weight: 600;
+    color: ${colors.mainOrange};
   }
+  ${(p) =>
+    p.disabled &&
+    css`
+      pointer-events: none;
+      opacity: 0.7;
+    `}
 `;
