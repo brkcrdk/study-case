cc5889ff847680f7f161a02ff6713fc9a72f763e
Burak <burak@90pixel.com>
2021-09-19 23:49:50 +0300

    Add onSearch event to filterStore.

--- src/store/filterStore.js
+++ src/store/filterStore.js
@@ -1,13 +1,30 @@
 import { useState, createContext } from "react";
+import initialData from "../data.json";
 
 const FilterContext = createContext();
 
 const FilterProvider = ({ children }) => {
+  const [data, setData] = useState(initialData);
   const [activePage, setActivePage] = useState(1);
+  const [search, setSearch] = useState("");
+
+  const onSearch = (searchValue) => {
+    setSearch(searchValue);
+    if (searchValue.length > 2) {
+      const filteredData = data.filter((product) => {
+        const productName = product.name.toLowerCase();
+        const expectedBrand = searchValue.toLowerCase();
+        return productName.includes(expectedBrand);
+      });
+      setData(filteredData);
+    } else {
+      setData(initialData);
+    }
+  };
 
   const onPageChange = (goTo) => setActivePage(goTo);
 
-  const values = { activePage, onPageChange };
+  const values = { data, search, onSearch, activePage, onPageChange };
 
   return (
     <FilterContext.Provider value={values}>{children}</FilterContext.Provider>
