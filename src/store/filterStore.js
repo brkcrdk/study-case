ada61ed74def8560a10b16cfd8fb02fa5999e7a7
Burak <burak@90pixel.com>
2021-09-20 12:53:44 +0300

    Restructure filter store.

--- src/store/filterStore.js
+++ src/store/filterStore.js
@@ -1,59 +1,92 @@
-import { useState, createContext } from "react";
+import { createContext, useReducer } from "react";
 import initialData from "../data.json";
 
 const FilterContext = createContext();
 
 const FilterProvider = ({ children }) => {
-  const [data, setData] = useState(initialData);
-  const [activePage, setActivePage] = useState(1);
-  const [search, setSearch] = useState("");
-  const [sort, setSort] = useState("ASC");
+  const initialStore = {
+    data: initialData,
+    activePage: 1,
+    search: "",
+    sort: "",
+  };
 
-  const onSearch = (searchValue) => {
-    setSearch(searchValue);
-    if (searchValue.length > 2) {
-      const filteredData = data.filter((product) => {
-        const productName = product.name.toLowerCase();
-        const expectedBrand = searchValue.toLowerCase();
-        return productName.includes(expectedBrand);
-      });
-      setData(filteredData);
-    } else {
-      setData(initialData);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "onSearch": {
+        if (action.payload.length > 2) {
+          const filteredData = state.data.filter((product) => {
+            const productName = product.name.toLowerCase();
+            const expectedBrand = action.payload.toLowerCase();
+            return productName.includes(expectedBrand);
+          });
+          return { ...state, search: action.payload, data: filteredData };
+        } else {
+          return { ...state, search: action.payload, data: initialData };
+        }
+      }
+      case "onPageChange": {
+        return { ...state, activePage: action.payload };
+      }
+      case "onSortChange": {
+        if (action.payload === state.sort) {
+          return { ...state, sort: "", data: initialData };
+        }
+        if (action.payload === "ASC") {
+          const ASC = state.data
+            .slice()
+            .sort((a, b) => a.name.localeCompare(b.name));
+          return { ...state, data: ASC, sort: action.payload };
+        }
+        if (action.payload === "DESC") {
+          const DESC = state.data
+            .slice()
+            .sort((a, b) => a.name.localeCompare(b.name));
+          return { ...state, data: DESC, sort: action.payload };
+        }
+        if (action.payload === "priceASC") {
+          const priceASC = state.data
+            .slice()
+            .sort((a, b) => a.realPrice - b.realPrice);
+          return { ...state, data: priceASC, sort: action.payload };
+        }
+        if (action.payload === "priceDESC") {
+          const priceDESC = state.data
+            .slice()
+            .sort((a, b) => b.realPrice - a.realPrice);
+          return { ...state, data: priceDESC, sort: action.payload };
+        }
+        return { ...state, sort: "", data: initialData };
+      }
+      default: {
+        return state;
+      }
     }
   };
 
-  const onPageChange = (goTo) => setActivePage(goTo);
+  const [test, dispatch] = useReducer(reducer, initialStore);
 
-  const onSortChange = (sortOption) => {
-    setSort(sortOption);
-    console.log(sortOption);
-    switch (sortOption) {
-      case "ASC": {
-        return data.sort((a, b) => a.name.localeCompare(b.name));
-      }
-      case "DESC": {
-        return data.sort((a, b) => b.name.localeCompare(a.name));
-      }
-      case "priceASC": {
-        return data.sort((a, b) => a.realPrice - b.realPrice);
-      }
-      case "priceDESC": {
-        return data.sort((a, b) => b.realPrice - a.realPrice);
-      }
-      default: {
-        return data;
-      }
+  const onSearch = (searchValue) => {
+    dispatch({ type: "onSearch", payload: searchValue });
+  };
+
+  const onPageChange = (goTo) => {
+    dispatch({ type: "onPageChange", payload: goTo });
+  };
+
+  const onSortChange = ({ filterType, value }) => {
+    if (filterType === "sort") {
+      dispatch({ type: "onSortChange", payload: value });
     }
   };
 
   const values = {
-    data,
-    search,
+    data: test.data,
+    search: test.search,
+    sort: test.sort,
+    activePage: test.activePage,
     onSearch,
-    activePage,
     onPageChange,
-    sort,
     onSortChange,
   };
 
