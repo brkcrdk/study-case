09c9e246bfcebe71e28a8b63475834ef3de7c87a
Burak <burak@90pixel.com>
2021-09-20 22:53:40 +0300

    Display item count.

--- src/components/Cart/ItemCount.jsx
+++ src/components/Cart/ItemCount.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import { colors } from "theme";
 
-function ItemCount() {
-  return <Count>4</Count>;
+function ItemCount({ count = 0 }) {
+  return <Count>{count}</Count>;
 }
 export default ItemCount;
 
