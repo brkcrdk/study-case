969fc3903397f5f8cf39918bcea7b62b88edddcd
Burak <burak@90pixel.com>
2021-09-21 12:39:03 +0300

    Add handleSortChange methods tests.

--- src/__tests__/filterMethods.test.js
+++ src/__tests__/filterMethods.test.js
@@ -39,10 +39,13 @@ const initialData = [
 ];
 
 const dummyData = {
-  state: { data: initialData, activePage: 2 },
+  state: { data: initialData, activePage: 2, sort: "", color: [], brand: [] },
 };
 const onSearch = (payload) =>
   handleSearch(dummyData.state, { payload }, initialData);
+
+const onSortChange = (payload) =>
+  handleSortChange(dummyData.state, { payload }, initialData);
 
 describe("Find counts testleri", () => {
   it("handleSearch 2 karakter girdikten sonra çalışmalı ve filtreleme resetlenmeli", () => {
@@ -54,10 +57,54 @@ describe("Find counts testleri", () => {
     expect(thirdResult.data).toHaveLength(5);
   });
 
-  it("handleSearch ile arama yapıldıktan sonra activePage 1 olmalı", () => {
+  it("handleSearch ile arama yapıldıktan sonra activePage resetleniyor", () => {
     const firstResult = onSearch("ap");
     expect(firstResult.activePage).toBe(2);
     const secondResult = onSearch("app");
     expect(secondResult.activePage).toBe(1);
+  });
+
+  it("handleChangeSort; ASC çalışıyor ve activePage resetleniyor", () => {
+    expect(dummyData.state.sort).toBe("");
+    const result = onSortChange("ASC");
+    expect(result.sort).toBe("ASC");
+    expect(result.activePage).toBe(1);
+    expect(result.data[4].brand).toBe("Xiaomi");
+  });
+
+  it("handleChangeSort; DESC çalışıyor ve activePage resetleniyor", () => {
+    expect(dummyData.state.sort).toBe("");
+    const result = onSortChange("DESC");
+    expect(result.sort).toBe("DESC");
+    expect(result.activePage).toBe(1);
+    expect(result.data[0].brand).toBe("Xiaomi");
+  });
+
+  it("handleChangeSort; priceASC çalışıyor ve activePage resetleniyor", () => {
+    expect(dummyData.state.sort).toBe("");
+    const result = onSortChange("priceASC");
+    expect(result.sort).toBe("priceASC");
+    expect(result.activePage).toBe(1);
+    expect(result.data[4].brand).toBe("Apple");
+    expect(result.data[0].brand).toBe("Xiaomi");
+  });
+
+  it("handleChangeSort; priceDESC çalışıyor ve activePage resetleniyor", () => {
+    expect(dummyData.state.sort).toBe("");
+    const result = onSortChange("priceDESC");
+    expect(result.sort).toBe("priceDESC");
+    expect(result.activePage).toBe(1);
+    expect(result.data[0].brand).toBe("Apple");
+    expect(result.data[3].brand).toBe("Huawei");
+  });
+
+  it("handleChangeSort; eğer seçili bir sort varsa ve tekrar aynı sort ile istek atılmışsa sort sıfırlanıyor ve activePage resetleniyor", () => {
+    const result = handleSortChange(
+      { ...dummyData.state, sort: "priceASC" },
+      { payload: "priceASC" },
+      initialData
+    );
+    expect(result.sort).toBe("");
+    expect(result.activePage).toBe(1);
   });
 });
