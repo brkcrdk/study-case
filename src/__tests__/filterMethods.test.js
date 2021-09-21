fbc0fbb87c761e663c576c29ebb0b94bbab3b0ee
Burak <burak@90pixel.com>
2021-09-21 12:56:47 +0300

    Add handleBrandChange and handleColorChange tests.

--- src/__tests__/filterMethods.test.js
+++ src/__tests__/filterMethods.test.js
@@ -47,6 +47,9 @@ const onSearch = (payload) =>
 const onSortChange = (payload) =>
   handleSortChange(dummyData.state, { payload }, initialData);
 
+const onColorChange = (payload) =>
+  handleColorChange(dummyData.state, { payload });
+
 describe("Find counts testleri", () => {
   it("handleSearch 2 karakter girdikten sonra çalışmalı ve filtreleme resetlenmeli", () => {
     const firstResult = onSearch("no");
@@ -106,5 +109,37 @@ describe("Find counts testleri", () => {
     );
     expect(result.sort).toBe("");
     expect(result.activePage).toBe(1);
+  });
+
+  it("handleColorChange; seçilen renk statete yoksa eklenir varsa çıkar ve active page resetlenir", () => {
+    const result = handleColorChange(dummyData.state, { payload: "kırmızı" });
+    expect(result.color).toContain("kırmızı");
+    expect(result.activePage).toBe(1);
+    const result2 = handleColorChange(
+      { ...result, activePage: 2 },
+      { payload: "sarı" }
+    );
+    expect(result2.color).toContain("sarı");
+    expect(result2.color).toContain("kırmızı");
+    expect(result2.activePage).toBe(1);
+    const result3 = handleColorChange(result2, { payload: "sarı" });
+    expect(result3.color).not.toContain("sarı");
+    expect(result3.color).toContain("kırmızı");
+  });
+
+  it("handleBrandChange; seçilen brand statete yoksa eklenir varsa çıkar ve active page resetlenir", () => {
+    const result = handleBrandChange(dummyData.state, { payload: "apple" });
+    expect(result.brand).toContain("apple");
+    expect(result.activePage).toBe(1);
+    const result2 = handleBrandChange(
+      { ...result, activePage: 2 },
+      { payload: "huawei" }
+    );
+    expect(result2.brand).toContain("apple");
+    expect(result2.brand).toContain("huawei");
+    expect(result2.activePage).toBe(1);
+    const result3 = handleBrandChange(result2, { payload: "apple" });
+    expect(result3.brand).not.toContain("apple");
+    expect(result3.brand).toContain("huawei");
   });
 });
